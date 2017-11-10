8734b7814ad0abcd07d2d6c2d51e81438416dd72
mitrichev <953288178@qq.com>
2017-11-10 17:10:48 +0800

    rocketmqtest

--- rocketmqtest/src/main/java/com/mitri/roketmq/ConsumerListen.java
+++ rocketmqtest/src/main/java/com/mitri/roketmq/ConsumerListen.java
@@ -20,12 +20,159 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConsumerListen {
 
-  @EventListener(condition = "#event.topic=='newTopic'")
+  @EventListener(condition = "#event.topic=='topic1'")
   public void testListen(RocketMqEvent event) {
     DefaultMQPushConsumer consumer = event.getConsumer();
     MessageExt messageExt = event.getMessageExt();
     try {
       System.out.println("正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic2'")
+  public void testListen2(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+"  正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic3'")
+  public void testListen3(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+" 正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic4'")
+  public void testListen4(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+ "  正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic5'")
+  public void testListen5(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+ "  正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic6'")
+  public void testListen6(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+ " 正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic7'")
+  public void testListen7(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+ " 正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
+        try {
+          consumer.sendMessageBack(messageExt, 1);
+        } catch (RemotingException | MQBrokerException | InterruptedException | MQClientException e1) {
+          e1.printStackTrace();
+          //消息进行定时重试
+        }
+      } else {
+        System.out.println("消息消费失败，定时重试");
+      }
+    }
+  }
+
+  @EventListener(condition = "#event.topic=='topic8'")
+  public void testListen8(RocketMqEvent event) {
+    DefaultMQPushConsumer consumer = event.getConsumer();
+    MessageExt messageExt = event.getMessageExt();
+    try {
+      System.out.println(event.getTopic()+ "  正在消费。。。"+ new String(messageExt.getBody(),"utf-8"));
     } catch (Exception e) {
       e.printStackTrace();
       if (messageExt.getReconsumeTimes() <= 1) {// 重复消费1次
